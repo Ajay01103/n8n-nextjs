16a638310e74040f8ab69bb47a94ff17794ac67e
Ajay <ajaysingh01103@gmail.com>
2025-12-23 14:46:57 +0530

    trpc_setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono, Roboto } from "next/font/google"
 import "./globals.css"
+import { TRPCReactProvider } from "@/trpc/client"
 
 const roboto = Roboto({ subsets: ["latin"], variable: "--font-sans" })
 
@@ -30,7 +31,7 @@ export default function RootLayout({
       className={roboto.variable}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+        <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
   )
